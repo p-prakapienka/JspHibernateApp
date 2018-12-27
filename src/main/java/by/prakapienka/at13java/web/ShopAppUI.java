3f0cbef2d60739df646d5208dc6f5625b1e3ec3a
p-prakapienka <prakapienka.work@gmail.com>
2018-12-27 15:59:07 +0300

    add theme

--- src/main/java/by/prakapienka/at13java/web/ShopAppUI.java
+++ src/main/java/by/prakapienka/at13java/web/ShopAppUI.java
@@ -3,12 +3,14 @@ package by.prakapienka.at13java.web;
 import by.prakapienka.at13java.web.view.ProductsView;
 import by.prakapienka.at13java.web.view.StartView;
 import by.prakapienka.at13java.web.view.UsersView;
+import com.vaadin.annotations.Theme;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@Theme("valo")
 public class ShopAppUI extends UI {
 
     private ConfigurableApplicationContext applicationContext;
