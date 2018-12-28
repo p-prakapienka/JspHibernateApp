8febd6f503b4bb11cb2f9ee2ac09acd80a074d73
p-prakapienka <prakapienka.work@gmail.com>
2018-12-28 15:24:53 +0300

    navigator annotation

--- src/main/java/by/prakapienka/at13java/web/ShopAppUI.java
+++ src/main/java/by/prakapienka/at13java/web/ShopAppUI.java
@@ -3,21 +3,16 @@ package by.prakapienka.at13java.web;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.spring.navigator.SpringNavigator;
+import com.vaadin.spring.annotation.SpringViewDisplay;
 import com.vaadin.ui.UI;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI
+@SpringViewDisplay
 @Theme("valo")
 public class ShopAppUI extends UI {
 
-    @Autowired
-    private SpringNavigator navigator;
-
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        navigator.init(this, this);
-        this.setNavigator(navigator);
     }
 
 }
